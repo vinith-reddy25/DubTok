4101ead38bf02b5a0a73131f4c10f87cf8e62ff6
vinith-reddy25 <64014016+vinith-reddy25@users.noreply.github.com>
2020-08-11 22:26:46 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ import { Image, Text, View } from 'react-native';
 function About() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>About Screen</Text>
+      <Text>Add Videos</Text>
     </View>
   );
 }
